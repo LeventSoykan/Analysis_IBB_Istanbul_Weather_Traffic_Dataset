c30fb46bb2a2c0ff67d37f18f16be8d3a8299b1a
Levent Soykan <levent_soykan@yahoo.com>
2022-03-19 15:48:16 +0300

    Create Data_Scrape.py
    Create a python script to get data from ibb dataset link and run sql commands

---
+++ data_scrape.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+import concurrent.futures
+import time
+
+def get_table_names(link):
+    source = requests.get(link).text
+    soup = BeautifulSoup(source, "lxml")
+    tables = [li['data-id'] for li in soup.find('ul',{'id':'data-list'}).find_all('li')]
+    return tables
+
+def run_sql(sql_query, table, fields=False):
+    sql_query = sql_query.replace('TABLE_PLACEHOLDER',table)
+    source = requests.get(f'https://data.ibb.gov.tr/api/3/action/datastore_search_sql?sql={sql_query}')
+    try:
+        result = pd.DataFrame(json.loads(source.text)['result']['records'])
+        if fields:
+            return pd.DataFrame(json.loads(source.text)['result']['fields'])
+    except KeyError:
+        result = json.loads(source.text)
+    return result
+
